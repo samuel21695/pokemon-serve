6f7bae9e00b8ef901a77375ef51b294faa5da388
samuel21695 <samuel21695@gmail.com>
2023-10-05 14:57:58 +0900

    readFile

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(function(request, response) {
   console.log(request.url);
   if(request.url === "/") {
     response.writeHead(200, {"Content-type": "text/html"});
 
-    let doc =`<html> <head> </head> <body> <h1> Welcome! You entered to a main page. </h1> </body> </html>`
-
-    response.end(doc)
-    
+    fs.readFile("./main.html", function(err, data) {
+      if (err) {
+        console.error('파일을 읽지 못했습니다.');
+      } else {
+        response.end(data)
+      }
+    })
   }
   if(request.url === "/serve") {
 
-    response.writeHead(200, {"Content-type": "text/html"});
-
-    let doc =`<html> <head> </head> <body> <h1> Welcome! You are in the sub page. </h1> </body> </html>`
-
-    response.end(doc)
+    fs.readFile("./sub.html", function(err, data) {
+      if (err) {
+        console.error('파일을 읽지 못했습니다.');
+      } else {
+        response.end(data)
+      }
+    })
   }
 })
 
