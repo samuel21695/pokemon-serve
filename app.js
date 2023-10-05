80d5fe901cd9114082ec88c5e210e7aedd276599
samuel21695 <samuel21695@gmail.com>
2023-10-05 14:30:00 +0900

    multipage application

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer(function(request, response) {
   console.log(request.url);
+  if(request.url === "/") {
+    console.log("메인입니다.")
+  }
+  if(request.url === "/serve")
+    console.log("서브입니다.")
 })
 
 server.listen(1234);
