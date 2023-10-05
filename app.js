6125bde4c4afea737621d4b0904d3792bf2c9c90
samuel21695 <samuel21695@gmail.com>
2023-10-05 15:19:42 +0900

    string && a tag

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer(function(request, response) {
   console.log(request.url);
   if(request.url === "/") {
     response.writeHead(200, {"Content-type": "text/html"});
 
-    fs.readFile("./main.html", function(err, data) {
-      if (err) {
-        console.error('파일을 읽지 못했습니다.');
-      } else {
-        response.end(data)
-      }
-    })
+    let doc =`<html> <head> </head> <body> <h1> Welcome! You entered to a main page. </h1> 
+    <a href="http://localhost:1234/serve">move to a sub page</a> </body> </html>`
+
+    response.end(doc)
+    
   }
   if(request.url === "/serve") {
 
-    fs.readFile("./sub.html", function(err, data) {
-      if (err) {
-        console.error('파일을 읽지 못했습니다.');
-      } else {
-        response.end(data)
-      }
-    })
+    response.writeHead(200, {"Content-type": "text/html"});
+
+    let doc =`<html> <head> </head> <body> <h1> Welcome! You are in the sub page. </h1> 
+    <a href="http://localhost:1234/">move to a main page</a> </body> </html>`
+
+    response.end(doc)
   }
 })
 
