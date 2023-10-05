2535beb9475e3956e918d85950a27a221f5fa5f8
samuel21695 <samuel21695@gmail.com>
2023-10-05 10:53:17 +0900

    writeHead && end

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 const server = http.createServer(function(request, response) {
-  console.log(request);
-  // console.log(typeof(response));
+  // console.log(request);
+  console.log(typeof(response));
+  response.writeHead(200, {"Content-type": "text/html"});
+  response.end("안녕!")
 })
 
 server.listen(1234);
