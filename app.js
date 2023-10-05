5c293775d8f7c1f369e1ad196e0ae009c9989a0d
samuel21695 <samuel21695@gmail.com>
2023-10-05 14:41:15 +0900

    string instead of console.log

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const http = require('http');
 const server = http.createServer(function(request, response) {
   console.log(request.url);
   if(request.url === "/") {
-    console.log("메인입니다.")
+    response.writeHead(200, {"Content-type": "text/html"});
+
+    let doc =`<html> <head> </head> <body> <h1> 메인페이지입니다. </h1> </body> </html>`
+
+    response.end(doc)
+    
   }
-  if(request.url === "/serve")
-    console.log("서브입니다.")
+  if(request.url === "/serve") {
+
+    response.writeHead(200, {"Content-type": "text/html"});
+
+    let doc =`<html> <head> </head> <body> <h1> 서브페이지입니다. </h1> </body> </html>`
+
+    response.end(doc)
+  }
 })
 
 server.listen(1234);
