fc272e8a56eb8b1a55b505cab63162217df23597
samuel21695 <samuel21695@gmail.com>
2023-10-05 16:39:40 +0900

    setTimeout but not working

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const http = require('http');
+const { setTimeout } = require('timers/promises');
 
 const server = http.createServer(function(request, response) {
   console.log(request.url);
   if(request.url === "/") {
-    response.writeHead(200, {"Content-type": "text/html"});
+    setTimeout(function() {
+      response.writeHead(200, {"Content-type": "text/html"});
+      
+      let doc =`<html> <head> </head> <body> <h1> Welcome! You are no longer in a main page. </h1> </body> </html>`
 
-    let doc =`<html> <head> </head> <body> <h1> Welcome! You entered to a main page. </h1> 
-    <a href="http://localhost:1234/serve">move to a sub page</a> </body> </html>`
-
-    response.end(doc)
-    
+      response.end(doc)
+    }, 50);
   }
+
+
   if(request.url === "/serve") {
 
     response.writeHead(200, {"Content-type": "text/html"});
